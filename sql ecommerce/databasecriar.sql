a3c2e66463bc8d79f52868283effabe1f5495eeb
Júlio César <juliocbizarria@gmail.com>
2025-11-10 22:37:55 -0300

    Delete sql ecommerce/databasecriar.sql

--- sql ecommerce/databasecriar.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS ecommerce;
-USE ecommerce;
-
-CREATE USER IF NOT EXISTS 'app_user'@'localhost' IDENTIFIED BY 'AppSenha123';
-GRANT ALL PRIVILEGES ON ecommerce.* TO 'app_user'@'localhost';
-FLUSH PRIVILEGES;
